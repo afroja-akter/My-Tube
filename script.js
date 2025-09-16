bbb4c8d7d8340ae4ded5fc3333ae9b43da39da9a
afroja <m.afroja267@gmail.com>
2025-09-17 00:12:34 +0600

    Catagory list style

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function dataCategories(categories){
     const categoryContainer = document.getElementById('category-container');
     for(let cat of categories){
         const categoryDiv = document.createElement('div')
-        categoryDiv.innerHTML = `<button class=" btn btn-sm">${cat.category}</button>`;
+        categoryDiv.innerHTML = `<button class=" btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>`;
         categoryContainer.appendChild(categoryDiv);
     }
 }
